775dc8cb8fdec514e08e2d5df8c35713da037511
Nesgc <nestorgarciac11@gmail.com>
2024-03-03 19:06:32 -0600

    1

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,14 @@
 <template>
-    <MainLayout>
+
         <div>Index</div>
         <Link href="/show">show Page</Link>
         <div>The message is {{ message }}</div>
-    </MainLayout>
+
 </template>
 
 <script setup>
-import MainLayout from "@/Layouts/MainLayout.vue";
+
+
 
 defineProps({ message: String });
 
