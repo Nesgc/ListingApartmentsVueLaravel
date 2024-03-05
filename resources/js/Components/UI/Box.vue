d451882d46e20eea63f14e9dae473e36285df9bc
Nesgc <nestorgarciac11@gmail.com>
2024-03-04 22:12:40 -0600

    box

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="border border-gray-200 dark:border-gray-600 rounded-md p-4 shadow-sm dark:text-gray-300"
+    >
+        <slot />
+    </div>
+</template>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,25 +1,26 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', listing.id)">
-                <ListingAddress :listing="listing"
-            /></Link>
-        </div>
-
-        <div>
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>
-        </div>
-        <h1 class="text-6xl text-amber-400 font-bold underline">
-            Hello world!
-        </h1>
-        <div>
-            <Link
-                :href="route('listing.destroy', listing.id)"
-                method="delete"
-                as="button"
-                >Delete</Link
-            >
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', { listing: listing.id })">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', { listing: listing.id })">
+                    Edit
+                </Link>
+            </div>
+            <div>
+                <Link
+                    :href="route('listing.destroy', { listing: listing.id })"
+                    method="DELETE"
+                    as="button"
+                >
+                    Delete
+                </Link>
+            </div>
+        </Box>
     </div>
 </template>
 
@@ -27,6 +28,7 @@
 import { Link } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import ListingAddress from "../../Components/ListingAddress.vue";
+import Box from "@/Components/UI/Box.vue";
 
 defineProps({
     listings: Array,
